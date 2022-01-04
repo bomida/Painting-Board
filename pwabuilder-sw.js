f2bcdc4c9c54ea41505dce27ffcf1cc22d6f1473
BomiDo <bm.do.bomhada@gmail.com>
2022-01-05 00:40:29 +0900

    EDIT

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'Paint Board'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'V1'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = '_V1'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
   '/Painting-Board/',                   // If you have separate JS/CSS files,
